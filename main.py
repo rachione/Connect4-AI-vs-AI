93f5781345df217acbc0e96176d819a520c67169
kyasuko <lychnussys@gmail.com>
2020-12-02 23:51:48 +0800

    feat: rename

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-import os
-import signal
+import subprocess
+from subprocess import Popen, PIPE
 import sys
-import pexpect
-import time
-from pexpect import popen_spawn
 
+p = Popen(["python", "core.py"],
+          stdin=PIPE,
+          universal_newlines=True)
 
-class Connect4:
-
-    def __init__(self):
-        self.proc = popen_spawn.PopenSpawn(
-            'GAME230-P1-Connect_Four.exe', logfile=sys.stdout.buffer)
-
-    def read(self):
-        self.proc.expect('.*: ')
-
-    def send(self, arg):
-        self.proc.sendline(arg)
-
-    def readAndSend(self, arg):
-        self.read()
-        self.send(arg)
-
-    def start(self):
-        args = ['4', '4', '4', '2', '2', '1']
-        for arg in args:
-            self.readAndSend(arg)
-        self.playChess()
-        self.kill()
-
-    def playChess(self):
-        self.readAndSend('1')
-
-    def kill(self):
-        os.kill(self.proc.pid, signal.SIGTERM)
-
-
-if __name__ == "__main__":
-    c4 = Connect4()
-    c4.start()
+p.stdin.close()
