f7bb68d8ec9ee79e5e4d5781d1228f761d5f7d3c
rachione <lychnussys@gmail.com>
2020-12-03 18:04:26 +0800

    Update core.py

--- core.py
+++ core.py
@@ -7,10 +7,9 @@ from pexpect import popen_spawn
 
 
 class Connect4:
-
     def __init__(self):
-        self.proc = popen_spawn.PopenSpawn(
-            'GAME230-P1-Connect_Four.exe', logfile=sys.stdout.buffer)
+        self.proc = popen_spawn.PopenSpawn('GAME230-P1-Connect_Four.exe',
+                                           logfile=sys.stdout.buffer)
 
     def read(self):
         self.proc.expect('.*: ')
@@ -26,11 +25,13 @@ class Connect4:
         args = ['4', '4', '4', '2', '2', '1']
         for arg in args:
             self.readAndSend(arg)
+        self.read()
         self.playChess()
         self.kill()
 
     def playChess(self):
-        self.readAndSend('1')
+        self.send('1')
+        self.read()
 
     def kill(self):
         os.kill(self.proc.pid, signal.SIGTERM)
